259faf081b538cbcb1f84c3ffc2a65d0d7841b39
Клочок Майя <klochok.maya@e-u.edu.ua>
2026-04-30 21:33:33 +0300

    final push

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,13 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    for i in 1..=n {
+        let spaces = " ".repeat(n - i);
+        let hashes = "#".repeat(i);
+        println!("{}{}", spaces, hashes);
+    }
+}
