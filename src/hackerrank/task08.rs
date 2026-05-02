c188b6f009df0a7be319d311b02f985719adfca3
Клочок Майя <klochok.maya@e-u.edu.ua>
2026-05-02 20:32:40 +0300

    Practical 7: task 08 Breaking the Records completed

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,27 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+pub fn get_total_x(a: &[i32], b: &[i32]) -> i32 {
+    let min_val = *a.iter().max().unwrap_or(&1);
+    let max_val = *b.iter().min().unwrap_or(&100);
+    let mut count = 0;
+
+    for x in min_val..=max_val {
+        let is_a_factor = a.iter().all(|&ai| x % ai == 0);
+        let is_factor_of_b = b.iter().all(|&bi| bi % x == 0);
+
+        if is_a_factor && is_factor_of_b {
+            count += 1;
+        }
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_total_x() {
+        let a = vec![2, 4];
+        let b = vec![16, 32, 96];
+        assert_eq!(get_total_x(&a, &b), 3);
+    }
+}
