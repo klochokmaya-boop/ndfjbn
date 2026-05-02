c3c5c058bde7e8dfdb86ab6695b8bf234a0ada89
Клочок Майя <klochok.maya@e-u.edu.ua>
2026-05-02 17:56:56 +0300

    Practical 3: task 6 kangaroo jumps completed

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,34 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        if x1 == x2 {
+            return String::from("YES");
+        }
+        return String::from("NO");
+    }
+
+    if (x2 - x1) % (v1 - v2) == 0 {
+        String::from("YES")
+    } else {
+        String::from("NO")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kangaroo_case_1() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+
+    #[test]
+    fn test_kangaroo_case_2() {
+        assert_eq!(kangaroo(0, 2, 5, 3), "NO");
+    }
+
+    #[test]
+    fn test_kangaroo_same_speed() {
+        assert_eq!(kangaroo(0, 2, 4, 2), "NO");
+    }
+}
