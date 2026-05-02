67dc876e420b21327647243f8f4cb740391f8962
Клочок Майя <klochok.maya@e-u.edu.ua>
2026-05-02 21:07:51 +0300

    Practical 8: task 09 Migratory Birds completed

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,39 @@
-// https://www.hackerrank.com/challenges/migratory-birds/problem
+pub fn breaking_records(scores: &[i32]) -> Vec<i32> {
+    if scores.is_empty() {
+        return vec![];
+    }
+
+    let mut min_score = scores[0];
+    let mut max_score = scores[0];
+    let mut min_count = 0;
+    let mut max_count = 0;
+
+    for &score in scores.iter().skip(1) {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
+        }
+    }
+
+    vec![max_count, min_count]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_breaking_records_case_1() {
+        let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+        assert_eq!(breaking_records(&scores), vec![2, 4]);
+    }
+
+    #[test]
+    fn test_breaking_records_case_2() {
+        let scores = vec![3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+        assert_eq!(breaking_records(&scores), vec![4, 0]);
+    }
+}
